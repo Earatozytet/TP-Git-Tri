7ab5f8ff256dffbca9a50dcb77effc81edefd5f4
mlo0olm <louis.giau.2009@gmail.com>
2026-05-15 16:13:49 +0200

    création de la fonction de lancer tri, mais il faut encore ajouter des tests pour voir si l'entrée est valide.
    mes modifs du tri à bulle, c'est juste pour tester

--- bulles.py
+++ bulles.py
@@ -1,2 +1,3 @@
 def bulles(liste):
-    pass
+    liste.append("🫧")
+    return liste
--- main.py
+++ main.py
@@ -9,16 +9,20 @@ dico_methode = {
 def lancer_tri():
     """fonction qui lance le tri grace aux fonctions codé"""
     print("tri lancé")
+    liste_nombres = [float(i.strip()) for i in entree.get().split(",")]
+    algo_utiliser = algo.get()
+    liste_triee = dico_methode[algo_utiliser](liste_nombres)
+    resulta.config(text=liste_triee)
 
 root = tk.Tk()
-root.geometry("300x300")
+root.geometry("200x300")
 root.title("Trieur de NSI")
 
 tk.Label(root, text="Liste à trier").pack()
 entree = tk.Entry(root)
 entree.pack()
 
-algo = tk.StringVar(value="Tri à bulle")
+algo = tk.StringVar(value="tri à bulle")
 tk.Label(root, text="Méthode : ").pack()
 menu_des_algo = tk.OptionMenu(root, algo,*dico_methode.keys())
 menu_des_algo.pack()
@@ -26,7 +30,7 @@ menu_des_algo.pack()
 bouton = tk.Button(root, text="lancer le tri", command=lancer_tri)
 bouton.pack()
 
-resulta = tk.Label(root, text="")
+resulta = tk.Label(root, text="",)
 resulta.pack()
 
 root.mainloop()
