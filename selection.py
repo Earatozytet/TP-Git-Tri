0518966782b45315d0b66b8827e797d523269864
Earatozytet <gaspard.lemonde@gmail.com>
2026-05-16 20:46:51 +0200

    Ajout du tri par sélection

--- selection.py
+++ selection.py
@@ -1,2 +1,8 @@
 def selection(liste):
-    pass
+    for i in range(len(liste) - 1):
+        mini = i
+        for j in range(i + 1, len(liste)):
+            if liste[j] < liste[mini]:
+                mini = j
+        liste[i], liste[mini] = liste[mini], liste[i]  # échange des valeurs
+    return liste
