839bad23215d2de0da8a3606792ebd5436ad1278
Earatozytet <gaspard.lemonde@gmail.com>
2026-05-16 21:00:39 +0200

    Ajout du tri bulle

--- bulles.py
+++ bulles.py
@@ -1,2 +1,13 @@
 def bulles(liste):
+    flag = 0
+    echange = 0
+    while flag == 0:
+        i = 0
+        flag = 1
+        while i+1 < len(liste):
+            if liste[i] > liste[i+1]:
+                liste[i],liste[i+1] = liste[i+1],liste[i]
+                flag = 0
+                echange += 1
+            i += 1
     return liste
