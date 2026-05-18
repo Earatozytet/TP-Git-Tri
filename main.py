887d0fd2ef4be20938d272cc4bfabb6a36ea7c2c
Earatozytet <gaspard.lemonde@gmail.com>
2026-05-18 23:40:09 +0200

    Amélioration du nom de la variable police afin de rendre le code plus compréhensible.

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import tkinter as tk
 import random, time
 import bulles, insertion, selection
 
-Pqfpagmseevtm = ('Comic Sans MS', 12) #police qui fait plaisir à gaspard même si elle est vraiment très moche
+font = ('Comic Sans MS', 12)
+
 dico_methode = {
     "Tri à bulle" : bulles.bulles,
     "Tri par insertion " : insertion.insertion,
@@ -80,52 +81,52 @@ root.title("Algorithmes de tri de NSI")
 frame1 = tk.Frame(root)
 frame1.pack(pady=5)
 
-tk.Label(frame1,text="Générer une liste aléatoire :",font=Pqfpagmseevtm).grid(row=0, column=0, columnspan=5)
+tk.Label(frame1, text="Générer une liste aléatoire :", font=font).grid(row=0, column=0, columnspan=5)
 
-tk.Label(frame1, text="Nombre :", font=Pqfpagmseevtm).grid(row=1, column=0)
-nb_element = tk.Entry(frame1, width=5, font=Pqfpagmseevtm)
+tk.Label(frame1, text="Nombre :", font=font).grid(row=1, column=0)
+nb_element = tk.Entry(frame1, width=5, font=font)
 nb_element.grid(row=1, column=1)
 
-tk.Label(frame1, text="Type :", font=Pqfpagmseevtm).grid(row=2, column=0)
+tk.Label(frame1, text="Type :", font=font).grid(row=2, column=0)
 type_element = tk.StringVar(value="int")
 menu_type_element = tk.OptionMenu(frame1, type_element, "int", "float")
-menu_type_element.config(font=Pqfpagmseevtm)
-menu_type_element["menu"].config(font=Pqfpagmseevtm)
+menu_type_element.config(font=font)
+menu_type_element["menu"].config(font=font)
 menu_type_element.grid(row=2, column=1)
 
-tk.Label(frame1, text="Min :", font=Pqfpagmseevtm).grid(row=1, column=3)
-min_element = tk.Entry(frame1, width=5, font=Pqfpagmseevtm)
+tk.Label(frame1, text="Min :", font=font).grid(row=1, column=3)
+min_element = tk.Entry(frame1, width=5, font=font)
 min_element.grid(row=1, column=4)
 
-tk.Label(frame1, text="Max :", font=Pqfpagmseevtm).grid(row=2, column=3)
-max_element = tk.Entry(frame1, width=5, font=Pqfpagmseevtm)
+tk.Label(frame1, text="Max :", font=font).grid(row=2, column=3)
+max_element = tk.Entry(frame1, width=5, font=font)
 max_element.grid(row=2, column=4)
 
-bouton_random = tk.Button(frame1,text="Générer",font=Pqfpagmseevtm,command=generer_liste)
+bouton_random = tk.Button(frame1, text="Générer", font=font, command=generer_liste)
 bouton_random.grid(row=3, column=0, columnspan=5)
 
 
-tk.Label(root, text="Liste à trier                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           :", font=Pqfpagmseevtm).pack()
-entree = tk.Text(root,width=25,height=8,bd=1,relief="solid", highlightthickness=0,font=Pqfpagmseevtm)
+tk.Label(root, text="Liste à trier                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           :", font=font).pack()
+entree = tk.Text(root, width=25, height=8, bd=1, relief="solid", highlightthickness=0, font=font)
 entree.pack()
 
 algo = tk.StringVar(value=list(dico_methode.keys())[0])
-tk.Label(root, text="Méthode :",font=Pqfpagmseevtm).pack()
+tk.Label(root, text="Méthode :", font=font).pack()
 
 frame2 = tk.Frame(root)
 frame2.pack()
 menu_des_algo = tk.OptionMenu(frame2, algo, *dico_methode.keys())
-menu_des_algo.config(font=Pqfpagmseevtm)
-menu_des_algo["menu"].config(font=Pqfpagmseevtm)
+menu_des_algo.config(font=font)
+menu_des_algo["menu"].config(font=font)
 menu_des_algo.pack(side="left")
 
-bouton_tri = tk.Button(frame2,text="Trier",bg="#0992E6",fg="white",command=lancer_tri,font=Pqfpagmseevtm,)
+bouton_tri = tk.Button(frame2, text="Trier", bg="#0992E6", fg="white", command=lancer_tri, font=font, )
 bouton_tri.pack(side="left")
-resultat = tk.Text(root,width=25,height=8,bd=1,relief="solid",highlightthickness=0,font=Pqfpagmseevtm,)
+resultat = tk.Text(root, width=25, height=8, bd=1, relief="solid", highlightthickness=0, font=font, )
 resultat.config(state="disabled")
 resultat.pack()
 
 
-affichage_temps = tk.Label(root, text="", font=Pqfpagmseevtm)
+affichage_temps = tk.Label(root, text="", font=font)
 affichage_temps.pack()
 root.mainloop()
